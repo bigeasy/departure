e051f65e5cc38f82f9e6d7323d71f1faa6a4b78e
Alan Gutierrez <alan@prettyrobots.com>
2018-11-25 02:45:24 -0600

    Rename `assert` to `okay`.

--- t/departure.t.js
+++ t/departure.t.js
@@ -1,13 +1,13 @@
 require('proof')(3, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Departure = require('..')
-    assert(Departure.compare(1, 1), null, 'equal')
-    assert(Departure.compare('1', 1), "ACTUAL '1'\nEXPECTED 1\nDIFF [ { kind: 'E', lhs: '1', rhs: 1 } ]", 'strict, equal')
+    okay(Departure.compare(1, 1), null, 'equal')
+    okay(Departure.compare('1', 1), "ACTUAL '1'\nEXPECTED 1\nDIFF [ { kind: 'E', lhs: '1', rhs: 1 } ]", 'strict, equal')
     Departure.raise(1, 1)
     try {
         Departure.raise('1', 1)
     } catch (error) {
-        assert(/^Assertion failure.$/m.test(error.message), 'exception raised')
+        okay(/^Assertion failure.$/m.test(error.message), 'exception raised')
     }
 }
