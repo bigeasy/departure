435f0e94bfa6f7a710980a111830989c7cd3e655
Alan Gutierrez <alan@prettyrobots.com>
2019-10-10 22:53:07 -0500

    Remove `deep-equal` dependency.
    It has so many dependencies itself.
    
    Closes #28.

--- departure.js
+++ departure.js
@@ -1,9 +1,38 @@
-const deepEqual = require('deep-equal')
 const diff = require('deep-diff')
+const assert = require('assert')
 const util = require('util')
+const OBJECT = {}
+
+const equal = exports.equal = function (left, right) {
+    if (Array.isArray(left)) {
+        if (!Array.isArray(right) || left.length != right.length) {
+            return false
+        }
+        for (let i = 0, I = left.length; i < I; i++) {
+            if (!equal(left[i], right[i])) {
+                return false
+            }
+        }
+        return true
+    } else if (typeof left === 'object') {
+        if (typeof right !== 'object') {
+            return false
+        }
+        assert(left.constructor === Object.prototype.constructor && right.constructor === Object.prototype.constructor)
+        for (var key in left) {
+            if (!(key in right) || !equal(left[key], right[key])) {
+                return false
+            }
+            return true
+        }
+    } else {
+        assert(!isNaN(left) && !isNaN(right) && left !== undefined && right !== undefined)
+        return left === right
+    }
+}
 
 exports.compare = function (actual, expected) {
-    if (!deepEqual(actual, expected, { strict: true })) {
+    if (!equal(actual, expected, { strict: true })) {
         const message = []
         message.push('ACTUAL ' + util.inspect(actual, null, Infinity))
         message.push('EXPECTED ' + util.inspect(expected, null, Infinity))
--- test/departure.t.js
+++ test/departure.t.js
@@ -1,12 +1,20 @@
-require('proof')(3, prove)
+require('proof')(10, prove)
 
 function prove (okay) {
-    const Departure = require('..')
-    okay(Departure.compare(1, 1), null, 'equal')
-    okay(Departure.compare('1', 1), "ACTUAL '1'\nEXPECTED 1\nDIFF [ { kind: 'E', lhs: '1', rhs: 1 } ]", 'strict, equal')
-    Departure.raise(1, 1)
+    const { compare, raise, equal } = require('..')
+    okay(!equal([], 1), 'array is not a number')
+    okay(!equal([ 1 ], []), 'array lengths not equal')
+    okay(!equal([ 1 ], [ 2 ]), 'arrays not equal')
+    okay(equal([ 1 ], [ 1 ]), 'arrays equal')
+    okay(!equal({}, 1), 'object is not a number')
+    const foo = {}
+    okay(!equal({ a: 1 }, { a: 2 }), 'objects not equal')
+    okay(equal({ a: 1 }, { a: 1 }), 'objects equal')
+    okay(compare(1, 1), null, 'equal')
+    okay(compare('1', 1), "ACTUAL '1'\nEXPECTED 1\nDIFF [ { kind: 'E', lhs: '1', rhs: 1 } ]", 'strict, equal')
+    raise(1, 1)
     try {
-        Departure.raise('1', 1)
+        raise('1', 1)
     } catch (error) {
         okay(/^Assertion failure.$/m.test(error.message), 'exception raised')
     }
